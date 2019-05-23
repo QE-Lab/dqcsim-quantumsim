5eab1825341405b3f5af0f1bc2fc1de9d60645f3
Jeroen van Straten <jeroen.van.straten@gmail.com>
2019-05-23 20:00:47 +0200

    Refactor source, add setuptools script

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import setup
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+setup(
+    name = "dqcsim-quantumsim",
+    version = "0.0.1",
+    author = "Jeroen van Straten",
+    author_email = "j.vanstraten-1@tudelft.nl",
+    description = "DQCsim backend for QuantumSim.",
+    license = "GPLv3",
+    keywords = "dqcsim quantumsim",
+    url = "https://github.com/jvanstraten/dqcsim-quantumsim",
+    long_description = read('README.md'),
+    long_description_content_type = 'text/markdown',
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Scientific/Engineering",
+    ],
+    packages = ['dqcsim_quantumsim'],
+    install_requires = ['dqcsim', 'quantumsim'],
+    data_files = [
+        ('bin', [
+            'data/bin/dqcsbequantumsim',
+        ]),
+    ],
+)
